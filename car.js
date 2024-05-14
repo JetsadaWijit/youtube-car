b340507d63340e9913544e2327d41148d7e8fbe8
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-14 14:05:49 +0000

    Create car.js

---
+++ car.js
@@ -0,0 +1,12 @@
+// brand, model, year, price - string
+// elite - boolean
+
+export class Car {
+    constructor(brand, model, year, price, elite) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.elite = elite;
+    }
+}
